29c792635c5c4f41da894b4515ede1d206148e68
hansputera <hanif@tslab.site>
2021-11-15 15:53:03 +0700

    remove SESSIONS from envrionment example file

--- api/stored-links.ts
+++ api/stored-links.ts
@@ -2,7 +2,7 @@ import {VercelRequest, VercelResponse} from '@vercel/node';
 import {matchLink} from '../lib/providers/util';
 import {client} from '../lib/redis';
 
-export default async (req: VercelRequest, res: VercelResponse) => {
+export default async (_: VercelRequest, res: VercelResponse) => {
   const keys = await client.keys('*');
 
   res.status(200).json(keys.filter((x) => matchLink(x)));
