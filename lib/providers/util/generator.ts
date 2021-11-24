65e7de0818435039e8e432cb896ca4a4cfce7b23
hansputera <hanifdwyputrasembiring@gmail.com>
2021-11-24 02:06:21 +0000

    bit changes

--- lib/providers/util/generator.ts
+++ lib/providers/util/generator.ts
@@ -1,6 +1,6 @@
-import { NodeVM } from 'vm2';
-import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
+import {NodeVM} from 'vm2';
+import {readFileSync} from 'node:fs';
+import {resolve} from 'node:path';
 
 /**
  * Generate key for ttsave.app
@@ -9,18 +9,18 @@ import { resolve } from 'node:path';
  * @return {string}
  */
 export const keyGeneratorTTSave = (token: string): string => {
-    const vm = new NodeVM({
-        'compiler': 'javascript',
-        'console': 'inherit',
-        'require': {
-        'external': true,
-        'root': './',
-        },
-    });
+  const vm = new NodeVM({
+    'compiler': 'javascript',
+    'console': 'inherit',
+    'require': {
+      'external': true,
+      'root': './',
+    },
+  });
 
 
-    return vm.run(readFileSync(
-        resolve(__dirname, 'tools', 'ttsave.js'), 'utf8'
-    ) +
-        'module.exports = key(`' + token + '`);', 'generator-ttsave.js')
-}; 
+  return vm.run(readFileSync(
+      resolve(__dirname, 'tools', 'ttsave.js'), 'utf8',
+  ) +
+        'module.exports = key(`' + token + '`);', 'generator-ttsave.js');
+};
