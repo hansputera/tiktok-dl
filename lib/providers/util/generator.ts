68ce429d145d07f7425d24296c6f51b0be7cdff5
hansputera <hanifdwyputrasembiring@gmail.com>
2021-11-24 08:00:57 +0000

    style: max-len code

--- lib/providers/util/generator.ts
+++ lib/providers/util/generator.ts
@@ -9,26 +9,28 @@ import {client as redisClient} from '../../redis';
  * @return {string | undefined}
  */
 export const keyGeneratorTTSave = async (token: string): Promise<string> => {
-    const vm = new NodeVM({
-        'compiler': 'javascript',
-        'console': 'inherit',
-        'require': {
-        'external': true,
-        'root': './',
-        },
+  const vm = new NodeVM({
+    'compiler': 'javascript',
+    'console': 'inherit',
+    'require': {
+      'external': true,
+      'root': './',
+    },
+  });
+
+  let scriptCache = await redisClient.get('ttsave-scripts');
+  if (!scriptCache) {
+    const response = await got('https://gist.githubusercontent.com/hansputera/' +
+                '70e33b8ac4a9beca4725c95cf517e8a6' +
+                    '/raw/9b1bf7f0eee16a69b38e2' +
+                        '03782bd46dae1611f8e/ttsave-gen.js', {
+      dnsCache: true,
     });
 
-    let scriptCache = await redisClient.get('ttsave-scripts');
-    if (!scriptCache) {
-            const response = await got('https://gist.githubusercontent.com/hansputera/' +
-                '70e33b8ac4a9beca4725c95cf517e8a6/raw/9b1bf7f0eee16a69b38e203782bd46dae1611f8e/ttsave-gen.js', {
-                    dnsCache: true,
-            });
+    await redisClient.set('ttsave-scripts', response.body, 'ex', 60 * 10);
+    scriptCache = response.body;
+  }
 
-            await redisClient.set('ttsave-scripts', response.body, 'ex', 60 * 10);
-            scriptCache = response.body;
-        }
-
-    return vm.run(scriptCache + 'module.exports = ' +
+  return vm.run(scriptCache + 'module.exports = ' +
         'key(`' + token + '`);', 'generator-ttsave.js');
 };
