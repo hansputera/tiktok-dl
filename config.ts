21d41ae2fff6cf9f58c17bd92f2d200b676505bf
hansputera <hanifdwyputrasembiring@gmail.com>
2022-01-14 09:28:08 +0000

    feat(middleware): add fastify-rate-limit middleware

--- config.ts
+++ config.ts
@@ -1,10 +1,9 @@
 export const rateLimitConfig = {
   'enable': true, // you can set it to 'false'
   /**
-     * How much rate limit count per x second(s)
-     * -is allowed? Default: 60 requests
+     * How much rate limit count is allowed?
      */
-  'maxRatelimitPerXSeconds': 60,
+  'maxRatelimit': 60,
   /**
      * Every x second(s), the ratelimit data will removed.
      * So, their ratelimit data will removed.
--- web/index.ts
+++ web/index.ts
@@ -1,14 +1,26 @@
 import {FastifyInstance, FastifyPluginOptions} from 'fastify';
+import FastifyRateLimit from 'fastify-rate-limit';
 import {PingRoute, StoredLinksRoute, DownloadRoute} from './routers';
+import {client} from '../lib';
+import {rateLimitConfig} from '../config';
 
 export default async (
     fastify: FastifyInstance,
     _: FastifyPluginOptions,
     done: (err?: Error) => void) => {
+  if (rateLimitConfig.enable) {
+    fastify.register(FastifyRateLimit, {
+      redis: client,
+      global: false,
+      max: rateLimitConfig.maxRatelimit,
+      ban: rateLimitConfig.maxRatelimit-1,
+      cache: rateLimitConfig.ratelimitTime*1_000,
+    });
+  }
   // routers
   new PingRoute(fastify);
   new StoredLinksRoute(fastify);
   new DownloadRoute(fastify);
-  
+
   done();
 };
