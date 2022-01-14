bdb13b52ef7fe15c7c5611f5d2fbc99f6d1f3242
hansputera <hanifdwyputrasembiring@gmail.com>
2022-01-14 09:55:40 +0000

    chore: change folder name from 'serverless' to 'api'

--- web/index.ts
+++ web/index.ts
@@ -4,13 +4,12 @@ export default async (
     fastify: FastifyInstance,
     _: FastifyPluginOptions,
     done: (err?: Error) => void) => {
-
   fastify.get('/docs', (_, reply) => {
-      reply.redirect(301, 'https://docs.tiktok-dl.tslab.site');
+    reply.redirect(301, 'https://docs.tiktok-dl.tslab.site');
   });
   // API
   fastify.register(import('./api'), {
-      prefix: '/api',
+    prefix: '/api',
   });
   done();
 };
