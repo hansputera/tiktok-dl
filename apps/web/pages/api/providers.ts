d7d11479a14ab940920f31fed8ae8e910a890536
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-05-23 01:45:11 +0000

    feat(apps.web#api.providers): add "params"
    Signed-off-by: GitHub <noreply@github.com>

--- apps/web/pages/api/providers.ts
+++ apps/web/pages/api/providers.ts
@@ -9,6 +9,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         name: p.resourceName(),
         url: p.client?.defaults.options.prefixUrl,
         maintenance: p.maintenance,
+        params: p.getParams() ?? {},
     }));
 
     return res.send(providers);
