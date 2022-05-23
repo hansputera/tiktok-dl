bce4d2f36d4adc8384ac090b974694d4f1454049
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-05-23 00:49:30 +0000

    style: prettier format
    Signed-off-by: GitHub <noreply@github.com>

--- apps/web/lib/redis.ts
+++ apps/web/lib/redis.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis';
 
 export const client = new Redis(
-    process.env.REDIS_URL ?? "redis://:@localhost:6379"
+    process.env.REDIS_URL ?? 'redis://:@localhost:6379',
 );
